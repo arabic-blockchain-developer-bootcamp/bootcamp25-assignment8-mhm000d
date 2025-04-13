fa2f16543f775fc12b3e0745fa1a3116a053b1e0
Mahmoud <mahmoudjamal01m@gmail.com>
2025-04-13 03:16:08 +0200

    Assignment 8

--- src/Assignment8.sol
+++ src/Assignment8.sol
@@ -4,14 +4,17 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";    
 import "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 // make this contract inherits from: ERC721URIStorage contract
-contract Assignment8 {
+contract Assignment8 is ERC721URIStorage, Ownable {
     // declare a private uint called _tokenIdCounter
+    uint256 private _tokenIdCounter;
 
     // pass name and symbol of the nft token collection
-    constructor() ERC721() {
-        
+    constructor() ERC721("Blokkat", "BLKT") Ownable(msg.sender) {
+        // initialize the token id counter to 0
+        _tokenIdCounter = 0;
     }
 
     // make this function visibility: external
@@ -19,13 +22,13 @@ contract Assignment8 {
     // restrict this function to be called only by the owner
         // Hint: You can use OpenZeppelin Ownable contract imported above
     
-    function mintNFT() {
+    function mintNFT(string memory josnURI) external onlyOwner {
         // call _mint to mint a new nft to the function caller
-
+        _mint(msg.sender, _tokenIdCounter);
         // set token uri to the token id using _setTokenURI
-
+        _setTokenURI(_tokenIdCounter, josnURI);
         // increment token id counter
-
+        _tokenIdCounter++;
     }
 
 }
